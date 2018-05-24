734409858397c2db8ecfb21ea66880207a326174
Vishal Pancholi <vishalpancholi02@gmail.com>
2018-05-25 00:37:04 +0530

    Used `with` keyword which is considered idiomatic for accessing resources.
    Apart from that, also made changes in comment format for uniformity.

--- tag.py
+++ tag.py
@@ -1,13 +1,18 @@
-#Python Program to get the last commit.
+'''
+Python Program to get the last commit.
+'''
 import datetime
 import os
 import sys
 from subprocess import check_output
 
-content = check_output(['git', 'rev-parse', '--short', 'HEAD'])  #This will store the output of the command to 'content'
+# This will store the output of the command to 'content'
+content = check_output(['git', 'rev-parse', '--short', 'HEAD'])  
 wd = os.getcwd()
-f=open("docs/image-version.md",'w')
-f.write("---\nid: version\ntitle: Docker Image\n---\nVersion: " + content[:6] + "\n") #Commit is written to file 
-now = datetime.datetime.now()
-f.write(str("\n" + now.strftime("%c"))) #Date is written to file.
-f.close()
+with open("docs/image-version.md",'w') as f:
+    
+    # Commit is written to file
+    f.write("---\nid: version\ntitle: Docker Image\n---\nVersion: " + content[:6] + "\n") 
+    now = datetime.datetime.now()
+    # Date is written to file.
+    f.write(str("\n" + now.strftime("%c"))) 
