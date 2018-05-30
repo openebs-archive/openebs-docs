1f1aacc7e134329fd3fc66b446fd5b295c94de08
Divy garg <divygarg95@gmail.com>
2018-05-30 21:58:29 +0530

    It is always safe to open a file using "with" because then we don't need to explicitly close the file.
    Signed-off-by: Divy Garg divygarg95@gmail.com

--- tag.py
+++ tag.py
@@ -6,8 +6,7 @@ from subprocess import check_output
 
 content = check_output(['git', 'rev-parse', '--short', 'HEAD'])  #This will store the output of the command to 'content'
 wd = os.getcwd()
-f=open("docs/image-version.md",'w')
-f.write("---\nid: version\ntitle: Docker Image\n---\nVersion: " + content[:6] + "\n") #Commit is written to file 
-now = datetime.datetime.now()
-f.write(str("\n" + now.strftime("%c"))) #Date is written to file.
-f.close()
+with open("docs/image-version.md",'w') as f:
+    f.write("---\nid: version\ntitle: Docker Image\n---\nVersion: " + content[:6] + "\n") #Commit is written to file 
+    now = datetime.datetime.now()
+    f.write(str("\n" + now.strftime("%c"))) #Date is written to file.
