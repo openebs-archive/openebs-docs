d0337a6be1fa457a7e1d13af10717484e381a802
isamrish <askmaurya48@gmail.com>
2020-04-22 13:21:02 +0530

    Fix broken links error on v1.0.0
    Signed-off-by: isamrish <askmaurya48@gmail.com>
    
    This commit will fix - combine code-block-css to custom css as well as rename longhorn image on jiva page

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -51,10 +51,7 @@ const siteConfig = {
     'https://buttons.github.io/buttons.js',
     'https://cdnjs.cloudflare.com/ajax/libs/clipboard.js/2.0.0/clipboard.min.js',
     '/js/code-blocks-buttons.js',
-  ],
-  separateCss: [
-    'static/css/code-blocks',
-  ],
+  ]
 };
 
 module.exports = siteConfig;
